5dd3a3564f1db4d611ff6d67db7096ddb3b0b119
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-28 22:33:21 +0100

    adds functions to validate email and phone, including regular expressions

--- app.js
+++ app.js
@@ -25,3 +25,25 @@ function validateZip() {
     zip.classList.remove('is-invalid');
   }
 }
+
+function validateEmail() {
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if(!re.test(email.value)) {
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if(!re.test(phone.value)) {
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
