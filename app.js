41a6ecb6c0fcb1d36653fa15835b76f05d333c9f
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-28 22:31:47 +0100

    adds event listeners

---
+++ app.js
@@ -0,0 +1,7 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+
